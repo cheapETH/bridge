bcf7d4a707c427329599a02db7813d329d72e42e
George Hotz <geohot@gmail.com>
2021-03-10 14:23:43 -0800

    checkpointer with tests

---
+++ contracts/Checkpointer.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0;
+pragma experimental ABIEncoderV2;
+
+import "hardhat/console.sol";
+
+/**
+ * @title Checkpointer
+ * @dev Allow users to attest to the state of the chain
+ */
+contract Checkpointer {
+  uint16 constant MINIMUM_BLOCK_LOOKBACK = 100;
+  uint16 constant MAX_TRUSTED = 100;
+
+  struct Checkpoint {
+    bytes32 hash;
+    uint savedBlockNumber;
+  }
+
+  mapping (uint => mapping (address => Checkpoint)) private att;
+  mapping (address => address) private trusted;
+  address[] trusters;
+
+  constructor() public {
+  }
+
+  /**
+   * Attest to a given block number having a hash
+   * Note: this transaction itself is signed by msg.sender
+   */
+  function attest(uint number, bytes32 blockHash) public {
+    require(block.number >= MINIMUM_BLOCK_LOOKBACK, "chain is too new");
+    require(number <= (block.number - MINIMUM_BLOCK_LOOKBACK), "block is too recent");
+
+    bytes32 hash = blockhash(number);
+    require(hash != 0, "blockhash not found");
+    require(hash == blockHash, "blockhash doesn't match attestation");
+
+    att[number][msg.sender] = Checkpoint({
+      hash: blockHash,
+      savedBlockNumber: block.number
+    });
+  }
+
+  function getBlockByNumber(uint blockNumber, address verifier) public view returns (Checkpoint memory) {
+    return att[blockNumber][verifier];
+  }
+
+  /**
+   * Designate a verifier to trust
+   */
+  function trust(address toTrust) public {
+    trusted[msg.sender] = toTrust;
+    if (trusters.length < MAX_TRUSTED) {
+      trusters.push(msg.sender);
+    } else {
+      // find lowest balance truster to replace
+      uint lowest = 0;
+      uint tbalance = trusters[0].balance;
+      for (uint i = 1; i < trusters.length; i++) {
+        if (trusters[i].balance < tbalance) {
+          lowest = i;
+          tbalance = trusters[i].balance;
+        }
+      }
+      trusters[lowest] = msg.sender;
+    }
+  }
+}
+
---
+++ test/Checkpointer.js
@@ -0,0 +1,51 @@
+const { expect } = require("chai");
+const lib = require("../scripts/lib");
+
+var rlp = require('rlp');
+
+describe("Checkpointer contract", function() {
+  before(async function() {
+    // "mine" 110 blocks
+    // TODO: is there a better way to do this?
+    [deployer] = await ethers.getSigners();
+    for (var i = 0; i < 110; i++) {
+      await deployer.sendTransaction({to: "0xd000000000000000000000000000000000000b1e", value:1});
+    }
+
+    latest_block_number = await ethers.provider.getBlockNumber();
+    expect(latest_block_number > 100);
+  });
+
+  beforeEach(async function() {
+    const CheckpointerFactory = await ethers.getContractFactory("Checkpointer");
+    Checkpointer = await CheckpointerFactory.deploy();
+  });
+
+  it("Submit checkpoint", async function() {
+    const submit_block = await ethers.provider.getBlock(latest_block_number - 100);
+    await Checkpointer.attest(submit_block.number, submit_block.hash);
+
+    const returned_block_hash = await Checkpointer.getBlockByNumber(submit_block.number, deployer.address);
+    expect(returned_block_hash.hash == submit_block.hash);
+    console.log(returned_block_hash);
+  });
+
+  it("Trust myself", async function() {
+    await Checkpointer.trust(deployer.address);
+  });
+
+  it("Submit checkpoint and send to BridgeAuthority", async function() {
+    const submit_block = await ethers.provider.getBlock(latest_block_number - 100);
+    await Checkpointer.attest(submit_block.number, submit_block.hash);
+
+    const returned_block_hash = await Checkpointer.getBlockByNumber(submit_block.number, deployer.address);
+    expect(returned_block_hash.hash == submit_block.hash);
+
+    const tx_block = await ethers.provider.getBlock(returned_block_hash.savedBlockNumber.toNumber());
+    tx_block.transactions.forEach(async function(x) {
+      const tx = await ethers.provider.getTransaction(x);
+      console.log(tx);
+    });
+  });
+
+});
