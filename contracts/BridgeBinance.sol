456b04c7ef326d7fa2043fae86150067d54896f8
Camisul <7963180+Camisul@users.noreply.github.com>
2021-03-16 05:07:19 +0300

    BridgeBinance: block validation working somewhat

--- contracts/BridgeBinance.sol
+++ contracts/BridgeBinance.sol
@@ -18,6 +18,7 @@ contract BridgeBinance {
 
   enum EncType {
     ENC_BYTES32,
+    ENC_BYTES8,
     ENC_ADDRESS,
     ENC_UINT,
     ENC_BYTES
@@ -25,23 +26,43 @@ contract BridgeBinance {
 
   function encodeRlpHeaderNoSign(bytes memory rlpHeader) internal pure returns (bytes memory) {
     Lib_RLPReader.RLPItem[] memory nodes = Lib_RLPReader.readList(rlpHeader);
-    EncType[15] memory encMap = [EncType.ENC_BYTES32,EncType.ENC_BYTES32,EncType.ENC_ADDRESS,EncType.ENC_BYTES32,EncType.ENC_BYTES32,EncType.ENC_BYTES32,EncType.ENC_BYTES,EncType.ENC_UINT,EncType.ENC_UINT,EncType.ENC_UINT,EncType.ENC_UINT,EncType.ENC_UINT,EncType.ENC_BYTES, EncType.ENC_BYTES32, EncType.ENC_UINT];
+    EncType[15] memory encMap = [
+      EncType.ENC_BYTES32, // parentHash
+      EncType.ENC_BYTES32, // sha3Uncles
+      EncType.ENC_ADDRESS, // miner aka coinbase
+      EncType.ENC_BYTES32, // stateRoot
+      EncType.ENC_BYTES32, // txHash
+      EncType.ENC_BYTES32, // receiptsRoot
+      EncType.ENC_BYTES,   // logsBloom
+      EncType.ENC_UINT,    // difficulty
+      EncType.ENC_UINT,    // Number ??? maybe blockNumber
+      EncType.ENC_UINT,    // gasLimit
+      EncType.ENC_UINT,    // gasUsed
+      EncType.ENC_UINT,    // timestamp
+      EncType.ENC_BYTES,   // extraData
+      EncType.ENC_BYTES32, // mixHash
+      EncType.ENC_BYTES    // nonce, should be exactly 8 bytes
+    ];
 
-    bytes[] memory raw = new bytes[](nodes.length);
+    bytes[] memory raw = new bytes[](nodes.length + 1);
+    // bsc chain id is 0x38
+    raw[0] = Lib_RLPWriter.writeUint(0x38);
     for(uint i = 0; i < nodes.length; i++) {
+      uint ri = i + 1;
+
       if(encMap[i] == EncType.ENC_BYTES32) {
         bytes32 tmp = Lib_RLPReader.readBytes32(nodes[i]);
-        raw[i] = Lib_RLPWriter.writeBytes(abi.encodePacked(tmp));
+        raw[ri] = Lib_RLPWriter.writeBytes(abi.encodePacked(tmp));
         continue;
       }
       if(encMap[i] == EncType.ENC_ADDRESS) {
         address tmp = Lib_RLPReader.readAddress(nodes[i]);
-        raw[i] = Lib_RLPWriter.writeAddress(tmp);
+        raw[ri] = Lib_RLPWriter.writeAddress(tmp);
         continue;
       }
       if(encMap[i] == EncType.ENC_UINT) {
         uint tmp = Lib_RLPReader.readUint256(nodes[i]);
-        raw[i] = Lib_RLPWriter.writeUint(tmp);
+        raw[ri] = Lib_RLPWriter.writeUint(tmp);
         continue;
       }
       if(encMap[i] == EncType.ENC_BYTES) {
@@ -50,7 +71,7 @@ contract BridgeBinance {
         if (i == 12) {
           tmp = Lib_BytesUtils.slice(tmp,0, tmp.length - 65);
         }
-        raw[i] = Lib_RLPWriter.writeBytes(tmp);
+        raw[ri] = Lib_RLPWriter.writeBytes(tmp);
         continue;
       }
     }
@@ -78,7 +99,30 @@ contract BridgeBinance {
       submitHeader(rlpHeaders[i]);
     }
   }
+  function splitSignature(bytes memory sig) public pure returns (bytes32 r, bytes32 s, uint8 v)
+  {
+      require(sig.length == 65, "invalid signature length");
 
+      assembly {
+          /*
+          First 32 bytes stores the length of the signature
+
+          add(sig, 32) = pointer of sig + 32
+          effectively, skips first 32 bytes of signature
+
+          mload(p) loads next 32 bytes starting at the memory address p into memory
+          */
+
+          // first 32 bytes, after the length prefix
+          r := mload(add(sig, 32))
+          // second 32 bytes
+          s := mload(add(sig, 64))
+          // final byte (first byte of the next 32 bytes)
+          v := byte(0, mload(add(sig, 96)))
+      }
+
+      // implicitly return (r, s, v)
+  }
   function submitHeader(bytes memory rlpHeader) public {
     bytes32 blockHash = keccak256(rlpHeader);
     uint blockNumber;
@@ -98,23 +142,16 @@ contract BridgeBinance {
 
     // TODO: validate block
     // see https://docs.binance.org/smart-chain/guides/concepts/consensus.html
-    uint8 v;
-    bytes32 r;
-    bytes32 s;
-    uint start = extraData.length-65+32; // +32 for length storage?
-    assembly {
-      let sig := add(extraData, start)
-      r := mload(add(sig, 0))
-      s := mload(add(sig, 32))
-      v := mload(add(sig, 33))  // actually the 64th byte
-    }
+    // also https://github.com/binance-chain/bsc/blob/master/consensus/parlia/parlia.go#L153
+
+    bytes memory slice = Lib_BytesUtils.slice(extraData, extraData.length - 65);
+    (bytes32 r, bytes32 s, uint8 v) = splitSignature(slice);
+    // bruh.
+    v = v + 27;
     bytes memory nonSignedHeader = encodeRlpHeaderNoSign(rlpHeader);
-    console.logBytes(nonSignedHeader);
-    // TODO: extract the correct part of the block header for this
-    bytes32 signedmsg = keccak256("");
+    bytes32 signedmsg = keccak256(nonSignedHeader);
     address signer = ecrecover(signedmsg, v, r, s);
-    console.log(signer, miner);
-    //require(signer == miner, "not signed by miner");
+    require(signer == miner, "not signed by miner");
 
     headers[blockNumber] = blockHash;
   }
--- test/BridgeBinance.js
+++ test/BridgeBinance.js
@@ -1,9 +1,69 @@
 const { expect } = require("chai");
 const lib = require("../scripts/lib");
-
+var utils = require('ethereumjs-util');
 var rlp = require('rlp');
 var Web3 = require('web3');
 var w3 = new Web3("https://bsc-dataseed.binance.org/");
+
+const sampleRlp = '0xf9021938a06a2d1e604855bb8a0a54a4e6d17dfd3b534297765c6db78e83582368a7817e20a01dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d4934794ea0a6e3c511bbd10f4519ece37dc24887e11b55da02efef6cc93385f05233ede5d749d2cd04eb38b045130c4ba1efc72e83b07503ea056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421b901000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002830174018401c9c38080845f4e27a7a0d883010002846765746888676f312e31332e34856c696e757800000000000000a00000000000000000000000000000000000000000000000000000000000000000880000000000000000';
+const sampleHeader = {
+  "parentHash":"0x6a2d1e604855bb8a0a54a4e6d17dfd3b534297765c6db78e83582368a7817e20",
+  "sha3Uncles":"0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
+  "miner":"0xea0a6e3c511bbd10f4519ece37dc24887e11b55d",
+  "stateRoot":"0x2efef6cc93385f05233ede5d749d2cd04eb38b045130c4ba1efc72e83b07503e",
+  "transactionsRoot":"0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
+  "receiptsRoot":"0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
+  "logsBloom":"0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+  "difficulty":"0x2",
+  "number":"0x17401",
+  "gasLimit":"0x1c9c380",
+  "gasUsed":"0x0",
+  "timestamp":"0x5f4e27a7",
+  "extraData":"0xd883010002846765746888676f312e31332e34856c696e757800000000000000edaa5052b9f0cca12979353619ce2d5d1f466e5d0f1a3c71cb2967c7bc7e780006d219d50c12dcff52836af3157f196f7cf83a4cbe61dc26d88a9b3b02791e9e00",
+  "mixHash":"0x0000000000000000000000000000000000000000000000000000000000000000",
+  "nonce":"0x0000000000000000",
+  "hash":"0x70903a8bb10d0e87065497525b06273c5f38eb7b6d554857bcbdf38b33157539"
+};
+const buf2hex = (x) => '0x'+x.toString('hex');
+const hex2buf = (x) => new Buffer(x.slice(2), 'hex');
+function handleRlpHeader(rlpHeader) {
+  let decoded = rlp.decode(rlpHeader);
+  const signature = decoded[12].slice(decoded[12].length - 65);
+  decoded[12] = decoded[12].slice(0, decoded[12].length - 65);
+  return {signature, rlp: rlp.encode([0x38, ...decoded]) };
+}
+function parseSig(sig) {
+  const r = sig.slice(0, 32);
+  const s = sig.slice(32,64);
+  // this is fucking retarded
+  let v = sig.slice(64, 65).readInt8() + 27; 
+  v = v.toString();
+  v = Buffer.from(v);
+  return {r,s,v};
+}
+describe("handleRlpHeader", function() {
+  it("Correctly encodes sample header", async function() {
+    const {signature, rlp} = handleRlpHeader(lib.getBlockRlp(sampleHeader));
+    expect(buf2hex(rlp)).to.equal(sampleRlp);
+  });
+  it("Correctly extracts the signature", async function() {
+    const {signature, rlp} = handleRlpHeader(lib.getBlockRlp(sampleHeader));
+    expect(buf2hex(signature)).to.equal('0xedaa5052b9f0cca12979353619ce2d5d1f466e5d0f1a3c71cb2967c7bc7e780006d219d50c12dcff52836af3157f196f7cf83a4cbe61dc26d88a9b3b02791e9e00');
+  });
+  it("Hash of rlp encoded payload is correct", async function() {
+    const {signature, rlp} = handleRlpHeader(lib.getBlockRlp(sampleHeader));
+    const keccak256 = Web3.utils.sha3(rlp);
+    expect(keccak256).to.equal('0xf472a3e5116fa0e1310e40f570943c453df78050ba57aa7486269bd8876094e9');
+  });
+  it("Correctly recovers pubkey", async function() {
+    const {signature, rlp} = handleRlpHeader(lib.getBlockRlp(sampleHeader));
+    //const msg = Web3.utils.sha3(rlp);
+    const {r,s,v} = parseSig(signature);
+    console.log({r,s,v});
+    const msg = Buffer.from('f472a3e5116fa0e1310e40f570943c453df78050ba57aa7486269bd8876094e9', 'hex')
+    utils.ecrecover(msg, v, r, s);
+  })
+});
 
 describe("BridgeBinance contract", function() {
   // deploying at latest so the getValidators() call works
@@ -26,15 +86,16 @@ describe("BridgeBinance contract", function() {
     const block = await Bridge.getBlockByNumber(STARTBLOCK);
     console.log(block);
   });
-  it("Does sometihn", async function() {
-    const add_block = await w3.eth.getBlock(STARTBLOCK-100);
-    console.log(add_block);
+  it("Does something", async function() {
+    const add_block = sampleHeader; //await w3.eth.getBlock(STARTBLOCK-100);
+    //console.log(add_block);
+    console.log('miner ', add_block['miner']);
     const block_rlp_headers = lib.getBlockRlp(add_block);
-    console.log(block_rlp_headers);
+    //const pub =  await w3.eth.personal.ecRecover(sha3, '0x'+sig.toString('hex'));
+    console.log();
     Bridge = await BridgeBinanceFactory.deploy(lib.getBlockRlp(genesis_block), validators);
 
     await Bridge.submitHeader(block_rlp_headers);
-    //console.log(block);
   });
   it("Look for not found block", async function() {
     Bridge = await BridgeBinanceFactory.deploy(lib.getBlockRlp(genesis_block), validators);
